0059371b33ce53e193bf6d312f18225573c2c36f
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-04-30 21:04:41 +0100

    Questions added

--- src/components/QuizScreen.js
+++ src/components/QuizScreen.js
@@ -1,11 +1,35 @@
 import { useState } from "react";
 import QuestionList from "../data/questions.json";
+import QuizResult from "./QuizResult.js";
+import Question from "./Question.js";
 
 function QuizScreen() {
     const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0);
     const [markedAnswers, setMarkedAnswers] = useState(new Array(QuestionList.length));
-    return (
+    const isQuestionEnd = currentQuestionIndex === QuestionList.length;
 
+    return (
+        <div className="quiz-screen">
+            {
+                isQuestionEnd ? (
+                    <QuizResult />
+                ) : (
+                    <Question 
+                        question={QuestionList[currentQuestionIndex]}
+                        totalQuestions={QuestionList.length}
+                        currentQuestion={currentQuestionIndex+1}
+                        setAnswer={(index) => {
+                            setMarkedAnswers((arr) => {
+                                let newArr = [...arr];
+                                newArr[currentQuestionIndex] = index;
+                                return newArr;
+                            });
+                            setcurrentQuestionIndex(currentQuestionIndex+1);
+                        }}
+                    />
+                )
+            }
+        </div>
     );
 }
 
