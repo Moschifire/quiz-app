68264f599bf9f722e678012800d0f3525c849e2e
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-05-02 18:41:33 +0100

    moving on to styling

--- src/components/QuizScreen.js
+++ src/components/QuizScreen.js
@@ -3,16 +3,33 @@ import QuestionList from "../data/questions.json";
 import QuizResult from "./QuizResult.js";
 import Question from "./Question.js";
 
-function QuizScreen() {
-    const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0);
+function QuizScreen({ retry }) {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [markedAnswers, setMarkedAnswers] = useState(new Array(QuestionList.length));
     const isQuestionEnd = currentQuestionIndex === QuestionList.length;
 
+    const calculateResult = () => {
+        let correct = 0;
+        QuestionList.forEach((question, index) => {
+            if(question.correctOptionIndex == markedAnswers[index]) {
+                correct++;
+            }
+        });
+        return {
+            total: QuestionList.length,
+            correct: correct,
+            percentage: Math.trunc((correct / QuestionList.length) * 100)
+        }
+    }
+    
     return (
         <div className="quiz-screen">
             {
                 isQuestionEnd ? (
-                    <QuizResult />
+                    <QuizResult 
+                        result={calculateResult()}
+                        retry={retry}
+                    />
                 ) : (
                     <Question 
                         question={QuestionList[currentQuestionIndex]}
@@ -21,10 +38,10 @@ function QuizScreen() {
                         setAnswer={(index) => {
                             setMarkedAnswers((arr) => {
                                 let newArr = [...arr];
-                                newArr[currentQuestionIndex] = index;
+                                newArr[currentQuestionIndex-1] = index;
                                 return newArr;
                             });
-                            setcurrentQuestionIndex(currentQuestionIndex+1);
+                            setCurrentQuestionIndex(currentQuestionIndex+1);
                         }}
                     />
                 )
